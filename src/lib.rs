b29e745d04e7373241863ade5e8fd20e2c249563
Midas Lambrichts <midaslamb@gmail.com>
2021-02-25 21:23:23 +0100

    Created first compiling version.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,243 @@
-#[derive(Debug, PartialEq, Eq)]
-pub struct Problem {}
+use std::ops::Deref;
+use std::sync::Arc;
+use std::{cell::RefCell, collections::HashMap};
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Solution {}
+pub struct Problem {
+    pub amount_of_seconds: u32,
+    pub bonus_points: u32,
 
-pub fn solve(_problem: Problem) -> Solution {
+    pub cars: Vec<Arc<RefCell<Car>>>,
+    pub streets: Vec<Arc<RefCell<Street>>>,
+    pub intersections: Vec<Intersection>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Car {
+    pub id: u32,
+    pub path_to_take: Vec<Arc<RefCell<Street>>>,
+    pub destination_reached_at: Option<u32>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Street {
+    pub name: String,
+    /// Vec of cars with the amount of time they still have to be on the road to reach the end.
+    pub cars: Vec<(Arc<RefCell<Car>>, u32)>,
+    pub time_to_travel: u32,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Intersection {
+    pub id: u32,
+    pub incoming_streets: Vec<Arc<RefCell<Street>>>,
+    pub outgoing_streets: Vec<Arc<RefCell<Street>>>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Solution {
+    pub traffic_lights: HashMap<u32, Vec<(String, u32)>>,
+}
+
+pub fn solve(mut problem: Problem) -> Solution {
+    let mut traffic_lights = HashMap::<u32, Vec<(String, u32)>>::new();
+
+    let single_intersections = problem
+        .intersections
+        .iter_mut()
+        .enumerate()
+        .filter(|(_, intersection)| intersection.incoming_streets.len() == 1)
+        .for_each(|(u, i)| {
+            traffic_lights.insert(
+                u as u32,
+                vec![(i.incoming_streets[0].borrow_mut().name.clone(), 1 as u32)],
+            );
+        });
+
+    Solution { traffic_lights }
+}
+
+pub fn simulate(mut problem: Problem, mut solution: Solution) {
+    for time_step in 0..problem.amount_of_seconds {
+        simulate_step(&mut problem, &mut solution, time_step);
+    }
+}
+
+pub fn simulate_step(problem: &mut Problem, solution: &mut Solution, time: u32) {
+    for intersection in problem.intersections.iter_mut() {
+        let car_passed_intersection = false;
+        for street in intersection.incoming_streets.iter() {
+            let can_cross = car_passed_intersection
+                && is_light_green(&solution, &intersection, &street.borrow_mut(), &time);
+            for (car, time_before_end) in street.borrow_mut().cars.iter_mut() {
+                if *time_before_end == 0 {
+                    let next_street_index = car
+                        .borrow_mut()
+                        .path_to_take
+                        .iter()
+                        .position(|r| r == street)
+                        .unwrap()
+                        + 1;
+                    if next_street_index < car.borrow_mut().path_to_take.len() {
+                        if can_cross {
+                            let next_street = &car.borrow_mut().path_to_take[next_street_index];
+                            let c = street.borrow_mut().cars.remove(
+                                street
+                                    .borrow_mut()
+                                    .cars
+                                    .iter()
+                                    .position(|c| c.0 == *car)
+                                    .unwrap(),
+                            );
+                            next_street
+                                .borrow_mut()
+                                .cars
+                                .push((c.0, next_street.borrow().time_to_travel));
+                            car.borrow_mut().destination_reached_at = Some(time);
+                        }
+                    } else {
+                        street.borrow_mut().cars.remove(
+                            street
+                                .borrow_mut()
+                                .cars
+                                .iter()
+                                .position(|c| c.0 == *car)
+                                .unwrap(),
+                        );
+                        car.borrow_mut().destination_reached_at = Some(time);
+                    }
+                } else {
+                    *time_before_end -= 1;
+                }
+            }
+        }
+    }
     todo!()
+}
+
+pub fn is_light_green(
+    solution: &Solution,
+    intersection: &Intersection,
+    street: &Street,
+    time: &u32,
+) -> bool {
+    let street_times = &solution.traffic_lights[&intersection.id];
+    let time = 0;
+    let mut street_index = 0;
+    let mut time_to_go = street_times[street_index].1;
+    for i in 0..time {
+        if time_to_go == 0 {
+            street_index = (street_index + 1) % street_times.len();
+            time_to_go = street_times[street_index].1;
+        }
+        time_to_go -= 1
+    }
+    return street_times[street_index].0 == street.name;
+}
+
+pub fn calculate_score(problem: Problem) -> u32 {
+    let mut score: u32 = 0;
+    for car in problem.cars {
+        if let Some(time_of_arrival) = car.borrow().destination_reached_at {
+            score += problem.bonus_points + (problem.amount_of_seconds - time_of_arrival)
+        }
+    }
+    score
 }
 
 #[cfg(test)]
 mod test {
 
+    use super::*;
     type Result = std::result::Result<(), Box<dyn std::error::Error>>;
     #[test]
     pub fn example_is_correct() -> Result {
+        let mut c1 = Arc::new(RefCell::new(Car {
+            id: 0,
+            path_to_take: vec![],
+            destination_reached_at: None,
+        }));
+        let mut c2 = Arc::new(RefCell::new(Car {
+            id: 1,
+            path_to_take: vec![],
+            destination_reached_at: None,
+        }));
+
+        let street1 = Arc::new(RefCell::new(Street {
+            name: "rue-de-londres".to_owned(),
+            cars: vec![(c1.clone(), 0)],
+            time_to_travel: 1,
+        }));
+
+        let street2 = Arc::new(RefCell::new(Street {
+            name: "rue-d-amsterdam".to_owned(),
+            cars: vec![],
+            time_to_travel: 1,
+        }));
+
+        let street3 = Arc::new(RefCell::new(Street {
+            name: "rue-d-athenes".to_owned(),
+            cars: vec![(c2.clone(), 0)],
+            time_to_travel: 1,
+        }));
+
+        let street4 = Arc::new(RefCell::new(Street {
+            name: "rue-de-rome".to_owned(),
+            cars: vec![],
+            time_to_travel: 2,
+        }));
+
+        let street5 = Arc::new(RefCell::new(Street {
+            name: "rue-de-moscou".to_owned(),
+            cars: vec![],
+            time_to_travel: 3,
+        }));
+
+        c1.get_mut().path_to_take = vec![
+            street1.clone(),
+            street2.clone(),
+            street5.clone(),
+            street4.clone(),
+        ];
+        c2.get_mut().path_to_take = vec![street3.clone(), street5.clone(), street1.clone()];
+
+        let intersection1 = Intersection {
+            id: 0,
+            incoming_streets: vec![street1.clone()],
+            outgoing_streets: vec![street2.clone()],
+        };
+        let intersection2 = Intersection {
+            id: 1,
+            incoming_streets: vec![street2.clone(), street3.clone()],
+            outgoing_streets: vec![street5.clone()],
+        };
+        let intersection3 = Intersection {
+            id: 2,
+            incoming_streets: vec![street5.clone()],
+            outgoing_streets: vec![street1.clone(), street4.clone()],
+        };
+        let intersection4 = Intersection {
+            id: 3,
+            incoming_streets: vec![],
+            outgoing_streets: vec![street3.clone()],
+        };
+
+        let cars = vec![c1, c2];
+        let streets = vec![street1, street2, street3, street4, street5];
+        let intersections = vec![intersection1, intersection2, intersection3, intersection4];
+
+        let problem = Problem {
+            amount_of_seconds: 6,
+            bonus_points: 1000,
+            cars: cars,
+            streets: streets,
+            intersections: intersections,
+        };
+
+        let s = solve(problem);
+        dbg!(s);
+        todo!();
+
         Ok(())
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use hash_code_2021::{solve, Problem, Solution};
+use hash_code_2021::*;
+use std::sync::Arc;
+use std::{cell::RefCell, collections::HashMap};
 use std::{fs::write, path::PathBuf};
 use structopt::StructOpt;
 
@@ -68,12 +70,125 @@ fn parse_problem_from_file<T: AsRef<std::path::Path>>(
     parse_problem(std::fs::read_to_string(path)?)
 }
 
-fn parse_problem(_input: String) -> Result<Problem, Box<dyn std::error::Error>> {
-    todo!();
+fn parse_problem(input: String) -> Result<Problem, Box<dyn std::error::Error>> {
+    let lines: Vec<&str> = input.lines().collect::<Vec<&str>>().clone();
+
+    let first_line: Vec<u32> = lines[0]
+        .split(' ')
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect();
+
+    let amount_of_seconds = first_line[0];
+    let number_intersections = first_line[1];
+    let number_streets = first_line[2];
+    let number_cars = first_line[3];
+    let bonus_points = first_line[4];
+
+    let mut streets = HashMap::<&str, Arc<RefCell<Street>>>::new();
+    let mut intersections = HashMap::<u32, Intersection>::new();
+    let mut cars = HashMap::new();
+
+    for i in 1..(number_streets + 1) as usize {
+        let line = lines[i].split(' ').collect::<Vec<_>>();
+        let intersection_start: u32 = line[0].parse().unwrap();
+        let intersection_end: u32 = line[1].parse().unwrap();
+        let street_name = line[2];
+        let time_to_travel = line[3].parse().unwrap();
+
+        // store street
+        let street = Arc::new(RefCell::new(Street {
+            name: street_name.to_owned(),
+            cars: Vec::new(),
+            time_to_travel: time_to_travel,
+        }));
+        streets.insert(street_name, street.clone());
+
+        // update intersection
+        if let Some(x) = intersections.get_mut(&intersection_start) {
+            x.outgoing_streets.push(street.clone());
+        } else {
+            intersections.insert(
+                intersection_start,
+                Intersection {
+                    id: intersection_start,
+                    incoming_streets: Vec::new(),
+                    outgoing_streets: vec![street.clone()],
+                },
+            );
+        }
+
+        if let Some(x) = intersections.get_mut(&intersection_end) {
+            x.incoming_streets.push(street.clone());
+        } else {
+            intersections.insert(
+                intersection_end,
+                Intersection {
+                    id: intersection_end,
+                    incoming_streets: vec![street.clone()],
+                    outgoing_streets: Vec::new(),
+                },
+            );
+        }
+    }
+
+    // Parse cars
+    for i in (number_streets + 1) as usize..(number_streets + 1 + number_cars) as usize {
+        let line = lines[i].split(' ').collect::<Vec<_>>();
+        let car_id: u32 = line[0].parse().unwrap();
+
+        let mut route = Vec::new();
+
+        for j in 1..line.len() {
+            route.push(streets.get(line[j]).unwrap().clone());
+        }
+
+        let car = Arc::new(RefCell::new(Car {
+            id: car_id,
+            path_to_take: route,
+            destination_reached_at: None,
+        }));
+
+        cars.insert(car_id, car.clone());
+        streets
+            .get(line[1])
+            .unwrap()
+            .clone()
+            .borrow_mut()
+            .cars
+            .push((car.clone(), 0));
+    }
+
+    Ok(Problem {
+        amount_of_seconds: amount_of_seconds,
+        bonus_points: bonus_points,
+        cars: cars.values().map(|x| x.clone()).collect(),
+        streets: streets.values().map(|x| x.clone()).collect(),
+        intersections: intersections.drain().map(|(_, x)| x).collect(),
+    })
 }
 
-fn solution_to_string(_solution: Solution) -> String {
-    todo!()
+fn solution_to_string(solution: Solution) -> String {
+    let mut lines = Vec::<String>::new();
+    lines.push(solution.traffic_lights.len().to_string());
+
+    lines.append(
+        &mut solution
+            .traffic_lights
+            .into_iter()
+            .map(|(u, v)| {
+                let temp = v
+                    .iter()
+                    .map(|a| format!("{} {}", a.0, a.1))
+                    .collect::<Vec<_>>();
+                let mut r: Vec<String> = vec![u.to_string(), v.len().to_string()];
+                r.extend_from_slice(&temp[..]);
+                r
+            })
+            .flatten()
+            .collect::<Vec<_>>(),
+    );
+
+    lines.join("\n")
 }
 
 #[cfg(test)]
@@ -83,6 +198,6 @@ mod test {
     #[test]
     fn read_example_statement() {
         let problem = parse_problem_from_file("input/testfile").unwrap();
-        assert_eq!(problem, Problem {});
+        //assert_eq!(problem, Problem {});
     }
 }
